1d681e3c39927b9284dfe9d58c1b6ce68b9ffe97
Yumeinaruu <lisovoistas@yandex.ru>
2024-07-09 19:22:47 +0300

    Initial commit

---
+++ src/test/java/org/krainet/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.krainet.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
