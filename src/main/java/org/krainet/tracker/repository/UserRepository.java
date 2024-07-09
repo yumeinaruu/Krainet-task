bcf47091de6512b739e3610e58043d32f5ab3112
Yumeinaruu <lisovoistas@yandex.ru>
2024-07-09 21:23:24 +0300

    Repositories modified

--- src/main/java/org/krainet/tracker/repository/UserRepository.java
+++ src/main/java/org/krainet/tracker/repository/UserRepository.java
@@ -4,6 +4,9 @@ import org.krainet.tracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
 }
