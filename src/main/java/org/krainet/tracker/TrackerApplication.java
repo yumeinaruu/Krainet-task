15a5e6741d122b0328b199c752512e347cc56376
Yumeinaruu <lisovoistas@yandex.ru>
2024-07-09 21:26:39 +0300

    Main class modified

--- src/main/java/org/krainet/tracker/TrackerApplication.java
+++ src/main/java/org/krainet/tracker/TrackerApplication.java
@@ -1,8 +1,18 @@
 package org.krainet.tracker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+        title = "Tracker system",
+        description = "For krainet trainee position",
+        contact = @Contact(name = "Lisavy Stanislau",
+                url = "https://github.com/yumeinaruu",
+                email = "stas.lisavoy@icloud.com")
+))
 @SpringBootApplication
 public class TrackerApplication {
 
